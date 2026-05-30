8dedac5512704d43e3aa08a12c06e576e23e7e17
alyatimiamnah <alyatimiamnah111@gmail.com>
2026-05-31 00:24:51 +0300

    Add Q3.cpp to calculate sum from 1 to 5

---
+++ Applications/Q3.cpp
@@ -0,0 +1,21 @@
+/*Write a C++ program that:
+Uses a for loop
+Calculates the sum of numbers from 1 to 5
+Prints the final sum*/
+
+
+
+
+void Q3() {
+  int sum = 0;
+  int i = 0;
+
+  for (int i = 0; i <= 5; i++) {
+
+    sum = sum + i;
+
+
+
+  }
+  std::cout << "Sum = " << sum << std::endl;
+}
