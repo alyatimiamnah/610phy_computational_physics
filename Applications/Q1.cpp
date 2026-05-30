f45d2de4cd38b9f2354fce239a1a74729c3692ef
alyatimiamnah <alyatimiamnah111@gmail.com>
2026-05-31 00:19:00 +0300

    Implement Q1 function to check even variables

---
+++ Applications/Q1.cpp
@@ -0,0 +1,13 @@
+/*.Use C++
+. Declare two variables
+. Print the result using OR (||)
+. The condition is true if one of the variables is even
+*/
+
+void Q1() {
+
+  int a = 4;
+  int b = 7;
+
+  std::cout << "Result " << (a % 2 == 0 || b % 2 == 0) << std::endl;
+}
