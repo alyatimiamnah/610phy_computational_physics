d7be6ce0b09d55a128ea28acd3137844a2e22e0b
alyatimiamnah <alyatimiamnah111@gmail.com>
2026-05-31 00:29:14 +0300

    Add Q5.h with length and width definitions
    Created a header file Q5.h with length and width variables.

---
+++ Applications/Q5.h
@@ -0,0 +1,10 @@
+/*
+
+  1.Create a header file named Rectangle.h.
+  2.  Define two variables in the header file:
+      length = 6
+          width = 4
+          3. Define area*/
+
+int length =6 ;
+int width = 4 ;
